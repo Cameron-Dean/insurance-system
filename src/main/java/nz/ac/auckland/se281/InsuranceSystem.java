bc384d52be73f9037a5343cc775c0d956a289cd4
Cameron Dean <camerondeannz@gmail.com>
2023-03-13 10:10:39 +1300

    Updated printDatabase method to print out header when there are no profiles

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,14 +1,23 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+
+  
+  ArrayList<Profile> profiles = new ArrayList<Profile>();
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
+    // check if there are any profiles
+    if (profiles.isEmpty()) {
+      // currently no profiles in database
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
+    }
   }
 
   public void createNewProfile(String userName, String age) {
